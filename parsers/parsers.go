4add17ea026dd060edec0478eb8dfa3f7f238bfc
Mike Decker <mrd999@gmail.com>
2019-07-12 07:50:10 -0400

    gofmt

--- parsers/parsers.go
+++ parsers/parsers.go
@@ -3,70 +3,69 @@
 package parsers
 
 import (
-    "encoding/json"
-    "log"
-    "time"
+	"encoding/json"
+	"log"
+	"time"
 )
 
 type Episode struct {
-    Name string
-    Season int
-    Number int
-    Airdate time.Time `json:"-"`
-    //AirdateString string `json:"airdate"`
-    Airstamp time.Time
+	Name    string
+	Season  int
+	Number  int
+	Airdate time.Time `json:"-"`
+	//AirdateString string `json:"airdate"`
+	Airstamp time.Time
 }
 
 type Show struct {
-    Name string
-    PrevURL string
-    NextURL string
+	Name    string
+	PrevURL string
+	NextURL string
 }
 
 type ShowJSON struct {
-    Name string `json:"name"`
-    Links struct {
-        PreviousEpisode struct {
-            Href string `json:"href"`
-        } `json:"previousepisode"`
-        NextEpisode struct {
-            Href string `json:"href"`
-        } `json:"nextepisode"`
-    } `json:"_links"`
+	Name  string `json:"name"`
+	Links struct {
+		PreviousEpisode struct {
+			Href string `json:"href"`
+		} `json:"previousepisode"`
+		NextEpisode struct {
+			Href string `json:"href"`
+		} `json:"nextepisode"`
+	} `json:"_links"`
 }
 
-func GetMidnight(t time.Time) (time.Time) {
-    return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.Local)
+func GetMidnight(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.Local)
 }
 
-func ParseEpisode(body string) (*Episode) {
-    e := new(Episode)
-    
-    if len(body) == 0 {
-        e.Airdate = time.Now().Add(time.Hour * 24 * 365 * 50)
-        return e
-    }
+func ParseEpisode(body string) *Episode {
+	e := new(Episode)
 
-    err := json.Unmarshal([]byte(body), e)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	if len(body) == 0 {
+		e.Airdate = time.Now().Add(time.Hour * 24 * 365 * 50)
+		return e
+	}
 
-    e.Airdate = GetMidnight(e.Airstamp.Local())
-    return e
+	err := json.Unmarshal([]byte(body), e)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	e.Airdate = GetMidnight(e.Airstamp.Local())
+	return e
 }
 
+func ParseShow(body string) *Show {
+	s := new(Show)
 
-func ParseShow(body string) (*Show) {
-    s := new(Show)
-    
-    var sj ShowJSON
-    if err := json.Unmarshal([]byte(body), &sj); err != nil {
-        log.Fatalln(err)
-    }
+	var sj ShowJSON
+	if err := json.Unmarshal([]byte(body), &sj); err != nil {
+		log.Fatalln(err)
+	}
 
-    s.Name = sj.Name
-    s.PrevURL = sj.Links.PreviousEpisode.Href
-    s.NextURL = sj.Links.NextEpisode.Href
-    return s
+	s.Name = sj.Name
+	s.PrevURL = sj.Links.PreviousEpisode.Href
+	s.NextURL = sj.Links.NextEpisode.Href
+	return s
 }
