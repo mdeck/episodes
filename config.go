d285c0c00cbd740fcd98000503d7684f393f9a90
Mike Decker <mrd999@gmail.com>
2019-07-13 10:57:30 -0400

    move show config to separate file

---
+++ config.go
@@ -0,0 +1,21 @@
+// config
+
+package main
+
+var imdbs = []string{
+	"tt2191671", // Elementary
+	"tt5296406", // Designated Survivor
+	"tt1595859", // Blue Bloods
+	"tt5691552", // The Orville
+	"tt1721666", // Sister Wives
+	"tt3502248", // Bosch
+	"tt5171438", // Star Trek Discovery
+	"tt4955642", // The Good Place
+	"tt5770786", // GLOW
+	"tt0475784", // Westworld
+	"tt4687880", // Goliath
+	"tt2575988", // Silicon Valley
+	"tt4158110", // Mr. Robot
+	"tt4574334", // Stranger Things
+	"tt1475582", // Sherlock
+}
