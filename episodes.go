ee27198bc731547fe98b3a0ac330bea30c127097
Mike Decker <mrd999@gmail.com>
2019-07-13 12:11:00 -0400

    get shows & episodes concurrently

--- episodes.go
+++ episodes.go
@@ -10,6 +10,7 @@ import (
 	"math"
 	"net/http"
 	"sort"
+	"sync"
 	"time"
 )
 
@@ -34,12 +35,15 @@ func main() {
 }
 
 func retrieveShowEpisodeInfo() []*ShowInfo {
-	// for each imdb, get ShowInfo & add to slice
 	results := make([]*ShowInfo, len(imdbs))
+	doneShow := make(chan int)
 	fmt.Printf("Loading.. ")
 	for idx, imdb := range imdbs {
+		go makeShowRequest(imdb, &results[idx], doneShow)
+	}
+	for idx, _ := range imdbs {
 		fmt.Printf("%v.. ", len(results)-idx)
-		results[idx] = getInfo(imdb)
+		<-doneShow
 	}
 	fmt.Printf("\n\n")
 	return results
@@ -74,14 +78,25 @@ func displayResults(results []*ShowInfo) {
 	}
 }
 
-func getInfo(imdb string) *ShowInfo {
-	var i ShowInfo
+func makeShowRequest(imdb string, ptrInfo **ShowInfo, doneShow chan int) {
+	info := new(ShowInfo)
 	url := "http://api.tvmaze.com/lookup/shows?imdb=" + imdb
-	s := parsers.ParseShow(getBody(url))
-	i.Name = s.Name
-	i.Prev = parsers.ParseEpisode(getBody(s.PrevURL))
-	i.Next = parsers.ParseEpisode(getBody(s.NextURL))
-	return &i
+	show := parsers.ParseShow(getBody(url))
+	info.Name = show.Name
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go makeEpisodeRequest(show.PrevURL, &info.Prev, &wg)
+	go makeEpisodeRequest(show.NextURL, &info.Next, &wg)
+	wg.Wait()
+
+	*ptrInfo = info
+	doneShow <- 1
+}
+
+func makeEpisodeRequest(url string, ptrEp **parsers.Episode, wg *sync.WaitGroup) {
+	*ptrEp = parsers.ParseEpisode(getBody(url))
+	wg.Done()
 }
 
 func getBody(url string) string {
